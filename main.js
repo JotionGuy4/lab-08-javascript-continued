b3be52b0e9564df6ae557e452a01737c3a64dcc8
Joel Gonzalez <jg22320@gmail.com>
2020-11-11 20:21:54 -0500

    Finished Lab Main

--- main.js
+++ main.js
@@ -1,35 +1,14 @@
-//alert('Outputing information.');
-//console.log('Outputing information');
-//console.log('Outputing information');
-/*
-var age = 31; 
-var name = 'Mr.X';
-var isCreditCardValid = true;
-var empty = null;
-var creditcardNumber;
+let even = 4;
+let odd = 3;
+let Digital = 100;
 
-console.log(age);
-console.log(name);
-console.log(isCreditCardValid);
-console.log(empty);
-console.log(creditcardNumber);
-*/
-//Credit 5 different variables
-//One variable has to be number
-//One variable has a string
-//One variable needs to be a boolean
-//One variable needs to be null or (EMPTY)
-//One variable has to be undefined
+let Him = 'Jotion'
 
-var fishes = 2674; 
-var PS = 'Playstation5';
-var money = false;
-var empty = null;
-var forest;
+const DigitalPlaystation = 499;
 
+console.log(even + odd);
+console.log('4' + '3');
 
-console.log(fishes);
-console.log(PS);
-console.log(money);
-console.log(empty);
-console.log(forest);
+console.log('Let ' + Him + ' Go');
+
+console.log(DigitalPlaystation * Digital);
