782f50d2daf7b1765b90c553e52d2cb832ff3100
Joel Gonzalez <jg22320@gmail.com>
2020-11-11 19:14:03 -0500

    Completed

---
+++ main.js
@@ -0,0 +1,35 @@
+//alert('Outputing information.');
+//console.log('Outputing information');
+//console.log('Outputing information');
+/*
+var age = 31; 
+var name = 'Mr.X';
+var isCreditCardValid = true;
+var empty = null;
+var creditcardNumber;
+
+console.log(age);
+console.log(name);
+console.log(isCreditCardValid);
+console.log(empty);
+console.log(creditcardNumber);
+*/
+//Credit 5 different variables
+//One variable has to be number
+//One variable has a string
+//One variable needs to be a boolean
+//One variable needs to be null or (EMPTY)
+//One variable has to be undefined
+
+var fishes = 2674; 
+var PS = 'Playstation5';
+var money = false;
+var empty = null;
+var forest;
+
+
+console.log(fishes);
+console.log(PS);
+console.log(money);
+console.log(empty);
+console.log(forest);
